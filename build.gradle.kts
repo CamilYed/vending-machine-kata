19ff4bbac9a9a347ea7a6bdd67254309f9720c9e
kamil.jedrzejuk <kamil.jedrzejuk@calamari.io>
2024-09-15 20:20:20 +0200

    Initialize project

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+}
+
+group = "camilyed.github.io"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
